8f8e431c7275547adc2d374689a9424b3ab02d19
Tawanda Muzanenhamo <tawandamuzanenhamo14@gmail.com>
2023-07-05 03:30:07 +0200

    add base app styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BreweryView from "./components/BreweryView/BreweryView.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/breweries/:name" element={<BreweryView />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
